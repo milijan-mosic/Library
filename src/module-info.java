8aab85a3f8037bab101776bab42526c19dca71e6
milijan-mosic <milijan.mosic@gmail.com>
2024-12-11 00:11:06 +0100

    Deleted comments

--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,6 @@ module library {
     requires java.sql;         // For SQLite
     requires jdatepicker;      // For JDBC
 
-    exports library;           // Exports your main package for use
-    exports library.models;    // Exports models package if needed externally
+    exports library;
+    exports library.models;
 }
