3f09e1a61fb567c77ba1d81d2a417965a44e22a0
Milijan Mosic <milijan.mosic@gmail.com>
2026-01-11 23:05:12 +0100

    Formatted code

--- src/library/utils/FileUtils.java
+++ src/library/utils/FileUtils.java
@@ -14,8 +14,7 @@ public class FileUtils extends FileOperations {
         executor.submit(() -> {
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
                 writer.write(content);
-                System.out.println("Content written to file successfully on thread: " 
-                                   + Thread.currentThread().getName());
+                System.out.println("Content written to file successfully on thread: " + Thread.currentThread().getName());
             } catch (IOException e) {
                 System.err.println("Error writing to file: " + e.getMessage());
             }
